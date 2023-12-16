2f85d61bf3ffe2eb1b6c1154fc527c2ee94babc4
aleksanderpanasiuk <olekp249@gmail.com>
2023-12-16 17:51:19 +0100

    added basic data scraping from debugger

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,53 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import { debug } from 'console';
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "bintreevisualised" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('bintreevisualised.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from BinTreeVisualised!');
 	});
 
+	vscode.commands.registerCommand("bintreevisualised.logValue", test);
+	vscode.commands.registerCommand("bintreevisualised.readValue", getValue);
+
 	context.subscriptions.push(disposable);
+
+	vscode.debug.onDidChangeActiveDebugSession(test);
+}
+
+function test() {
+	console.log("debug changed");
+}
+
+async function getValue() {
+	const session = vscode.debug.activeDebugSession;
+	const response_thread = await session?.customRequest("threads");
+
+	if (!response_thread) {
+		return;
+	}
+
+	let threadId: number = response_thread.threads[0].id;
+
+	const response_frame = await session?.customRequest("stackTrace", { "threadId": threadId });
+	const frameId = response_frame.stackFrames[0].id;
+
+	const response_variable = await session?.customRequest("evaluate", {"expression": "root", "frameId": frameId});
+	let value: string = response_variable?.result;
+	let type: string = response_variable?.type;
+	let reference: number = response_variable?.variablesReference;
+
+	console.log(value);
+	console.log(type);
+	console.log(reference);
+
+	const response_varaibles = await session?.customRequest("variables", {"variablesReference": reference});
+
+	console.log(response_varaibles);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	console.log("deactiavted");
+}
