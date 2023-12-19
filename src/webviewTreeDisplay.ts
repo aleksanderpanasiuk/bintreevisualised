b9f94ac2199aa7d37644775fa34b03cb637aba5c
aleksanderpanasiuk <olekp249@gmail.com>
2023-12-19 10:33:37 +0100

    Implemented function calculating tree nodes positions

--- src/webviewTreeDisplay.ts
+++ src/webviewTreeDisplay.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import { TreeNode } from "./treenode";
+import internal from 'stream';
 
 
 export function displayTree(root: TreeNode | null, maxTreeDepth: number) {
@@ -10,7 +11,7 @@ export function displayTree(root: TreeNode | null, maxTreeDepth: number) {
 	  {}
 	);
 
-	panel.webview.html = getWebviewContent(getTree(root, 0));
+	panel.webview.html = getWebviewContent(getTree(root, maxTreeDepth, [0, 1]));
 
 	panel.onDidDispose(
 	  () => {
@@ -32,10 +33,12 @@ function getWebviewContent(tree: [string, number]): string {
         <style>
             node {
                 border-radius: 50%;
-                width: 20px;
-                height: 20px;
-                padding: 20px;
+                width: 15px;
+                height: 15px;
+                padding: 15px;
                 background: #48c268;
+                text-align: center;
+                position: fixed;
             }
         </style>
 
@@ -45,22 +48,25 @@ function getWebviewContent(tree: [string, number]): string {
 }
 
 
-function getTree(root: TreeNode | null, level: number): [string, number] {
+function getTree(root: TreeNode | null, maxTreeDepth: number, position: [number, number]): [string, number] {
     if (!root) {
-        return ["", level];
+        return ["", 0];
     }
 
     let result: string = "";
 
-    result += `<node> ${root.val} </node> <br>`;
+    let x: number = position[1] * 50 * Math.pow(2, maxTreeDepth) * 1.618 / (Math.pow(2, position[0])+1);
+    let y: number = position[0] * 100 + 30;
 
-    let left: [string, number] = getTree(root.left, level+1);
+    result += `<node style="top: ${y}px; left: ${x}px;"> ${root.val} </node> <br>`;
+
+    let left: [string, number] = getTree(root.left, maxTreeDepth, [position[0]+1, 2*position[1] - 1]);
     result += left[0];
 
-    let right: [string, number] = getTree(root.right, level+1);
+    let right: [string, number] = getTree(root.right, maxTreeDepth, [position[0]+1, 2*position[1]]);
     result += right[0];
 
-    let maxLevel: number = Math.max(level, left[1], right[1]);
+    let maxLevel: number = Math.max(position[0], left[1], right[1]);
 
 	return [result, maxLevel];
 }
